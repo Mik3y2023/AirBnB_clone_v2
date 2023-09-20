27c300168afc0f0b1e82da7192b7632dea7ca84e
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-09-20 09:14:12 -0700

    create database fix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,11 @@
--- Prepares a MySQL Test server for the project.
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
+-- Prepares a MySQL server for the project:
+-- Set ALL privileges on the database `hbnb_dev_db` to the `hbnb_dev` user.
+-- Set SELECT privileges on the database `performance_schema` to the `hbnb_dev` user.
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+DROP USER IF EXISTS 'hbnb_dev'@'localhost';
+CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+
